807d95e5ee46053fd5f8a2e417492b99d1865731
Martin McNickle <martin@floatapp.com>
2020-12-04 17:20:12 +0000

    Part 1 complete.

---
+++ day3/run_journey.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'matrix'
+
+require 'awesome_print'
+
+require_relative 'toboggan_map'
+require_relative 'journey'
+
+tree_data = File.read('map.txt')
+map = TobogganMap.new(tree_data)
+
+journey = Journey.new(map)
+slope = Vector[3, 1]
+
+number_of_trees = journey.count_trees(slope)
+
+puts "Number of trees: #{number_of_trees}"
