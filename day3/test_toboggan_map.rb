864a7c1dd90ffc7db541a158aa004824d6f17306
Martin McNickle <martin@floatapp.com>
2020-12-04 16:48:01 +0000

    Convert to spec

--- day3/test_toboggan_map.rb
+++ day3/test_toboggan_map.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'minitest/spec'
 
 require_relative 'toboggan_map'
 
-class TobogganMapTest < Minitest::Test
-  def test_dimensions
+describe TobogganMap do
+  let(:map) do
     tree_data = <<~TREEDATA
       ..##.......
       #...#...#..
@@ -20,73 +21,40 @@ class TobogganMapTest < Minitest::Test
       .#..#...#.#
     TREEDATA
 
-    map = TobogganMap.new(tree_data)
-
-    assert_equal(map.width, 11)
-    assert_equal(map.height, 11)
+    TobogganMap.new(tree_data)
   end
 
-  def test_tree?
-    tree_data = <<~TREEDATA
-      ..##.......
-      #...#...#..
-      .#....#..#.
-      ..#.#...#.#
-      .#...##..#.
-      ..#.##.....
-      .#.#.#....#
-      .#........#
-      #.##...#...
-      #...##....#
-      .#..#...#.#
-    TREEDATA
-
-    map = TobogganMap.new(tree_data)
-
-    assert(map.tree?(0, 0) == false)
-    assert(map.tree?(1, 2) == true)
+  describe '#width' do
+    it 'returns the width of the map' do
+      assert_equal(map.width, 11)
+    end
   end
 
-  def test_tree_out_of_bounds_y
-    tree_data = <<~TREEDATA
-      ..##.......
-      #...#...#..
-      .#....#..#.
-      ..#.#...#.#
-      .#...##..#.
-      ..#.##.....
-      .#.#.#....#
-      .#........#
-      #.##...#...
-      #...##....#
-      .#..#...#.#
-    TREEDATA
-
-    map = TobogganMap.new(tree_data)
-
-    map.tree?(0, 10)
-    assert_raises(ArgumentError) { map.tree?(0, 11) }
-    assert_raises(ArgumentError) { map.tree?(0, 12) }
+  describe '#height' do
+    it 'returns the height of the map' do
+      assert_equal(map.height, 11)
+    end
   end
 
-  def test_tree_loops_x
-    tree_data = <<~TREEDATA
-      ..##.......
-      #...#...#..
-      .#....#..#.
-      ..#.#...#.#
-      .#...##..#.
-      ..#.##.....
-      .#.#.#....#
-      .#........#
-      #.##...#...
-      #...##....#
-      .#..#...#.#
-    TREEDATA
+  describe '#tree' do
+    it 'returns if a tree exists at the coordinates' do
+      assert(map.tree?(0, 0) == false)
+      assert(map.tree?(1, 2) == true)
+    end
 
-    map = TobogganMap.new(tree_data)
+    describe 'when y is out of bounds' do
+      it 'raises an ArgumentError' do
+        map.tree?(0, 10)
+        assert_raises(ArgumentError) { map.tree?(0, 11) }
+        assert_raises(ArgumentError) { map.tree?(0, 12) }
+      end
+    end
 
-    assert_equal(map.tree?(0, 0), map.tree?(11, 0))
-    assert_equal(map.tree?(2, 0), map.tree?(13, 0))
+    describe 'when x is out of bounds' do
+      it 'loops around in the x direction' do
+        assert_equal(map.tree?(0, 0), map.tree?(11, 0))
+        assert_equal(map.tree?(2, 0), map.tree?(13, 0))
+      end
+    end
   end
 end
