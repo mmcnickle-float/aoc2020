78d667d10ada11a52681a20b4813a1a4c84b7046
Martin McNickle <martin@floatapp.com>
2020-12-04 18:15:48 +0000

    Add file to parse the passport files into Passports.

---
+++ day4/passport_file.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'awesome_print'
+
+class PassportFile
+  def initialize(file)
+    @file = file
+  end
+
+  def passports
+    passport_data_strings = file.read.split(/\n\n/)
+
+    passport_data_strings.map do |data_string|
+      Passport.parse(data_string)
+    end
+  end
+
+  private
+
+  attr_reader :file
+end
