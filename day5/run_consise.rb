42478302998b341bf17e01afd142940cf3d57cbf
Martin McNickle <martin@floatapp.com>
2020-12-05 15:25:35 +0000

    change filename

--- day5/run_consise.rb
+++
@@ -1,3 +0,0 @@
-seat_ids = File.read('codes.txt').lines.map { |c| c.gsub(/[FL]/, '0').gsub(/[BR]/, '1').to_i(2) }
-puts "Max seat id: #{seat_ids.max}"
-puts "Missing seat id: #{((seat_ids.min..seat_ids.max).to_a - seat_ids).first}"
